441443737abcd610cfb89975a87ea102e3e3b35b
DoisD <biel-2d_lima@hotmail.com>
2017-08-17 02:49:36 -0300

    Alterações feitas por Antony para correção de interface.

--- application/views/cadastro_producao.php
+++ application/views/cadastro_producao.php
@@ -4,31 +4,29 @@
         <p>Preencha os dados da produção que deseja cadastrar.</p>
     </div>
 
-    <div class="col-md-12">
-        <form class="form-control" action="<?= base_url() ?>producao/cadastro">
-            <br />
-            <br />
-            <br />
-            <div class="col-md-8">
-                <div class="form-group">
+    <div class="panel panel-default col-md-offset-2 col-md-8">
+        <form action="<?= base_url() ?>producao/cadastro">
+            <br>
+            <div class="col-md-12">
+                <div class="col-md-12 form-group">
                     <label for="InputTitulo">Título da Produção*</label>
                     <input type="text" class="form-control" id="InputTitulo" placeholder="Título">
                 </div>
-                <div class="form-group">
+                <div class="col-md-12 form-group">
                     <label for="InputAutor">Autor(es)*</label>
                     <input type="text" class="form-control" id="InputAutor" placeholder="Autor(es)">
                 </div>
-                <div class="form-group">
+                <div class="col-md-12 form-group">
                     <label for="InputDescricao">Descrição*</label>
                     <textarea class="form-control" rows="3"></textarea>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-6">
                     <div class="form-group">
                         <label for="InputTamanho">Tamanho do Arquivo*</label>
                         <input type="number" class="form-control" id="InputTamanho">
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-6">
                     <div class="form-group">
                         <label for="InputArea">Área de Interesse</label>
                         <select class="form-control">
@@ -38,8 +36,8 @@
                         </select>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-4">
+            
+            <div class="col-md-6">
                 <div class="form-group">
                     <label for="TipoProducao">Tipo de Produção</label>
                     <select class="form-control">
@@ -52,7 +50,13 @@
                     </select>
                 </div>
             </div>
-            <button type="submit" class="btn btn-default">Cadastrar</button>
+            <div class="col-md-12">
+                <button type="submit" class="btn btn-success col-md-6">Cadastrar</button>
+            </div>
+                <div class="col-md-12"><br><br></div>
+            </div>
         </form>
+
     </div>
+
 </div><!-- /.container -->
